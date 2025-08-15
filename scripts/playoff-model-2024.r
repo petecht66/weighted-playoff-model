5715ec255d6aff983d2b23ca5941cb4b8266c0b5
Peter <petecht66gmail.com>
2025-08-14 23:43:45 -0400

    starting temporary Tier 2 and Tier 3 lists

--- scripts/playoff-model-2024.r
+++ scripts/playoff-model-2024.r
@@ -9,8 +9,15 @@ race_data_2024
 # Tier 1: Crown jewels and championship finale
 Tier1 <- list("DAYTONA 500", "Coca-Cola 600", "Brickyard 400 Presented by PPG", "Cook Out Southern 500", "NASCAR Cup Series Championship")
 
+# Tier 2: Next most important races
+Tier2 <- list("XFINITY 500", "Bass Pro Shops Night Race", "Goodyear 400", "FireKeepers Casino 400")
+
+# Tier 3: More important than a regular race
+
+# Tier 4 is the remaining races, list does not need to be made
+
 # create 2024 playoff model function
-weighted_points_2024 <- function(Race, Pts){
-    if()
-    return(weighted_points)
-}
+# weighted_points_2024 <- function(Race, Pts){
+#    if()
+   # return(weighted_points)
+# }
